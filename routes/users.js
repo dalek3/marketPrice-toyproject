8b7d949a3554a16e277aac6c5c9ee52552424712
dalek3 <lina1318@gmail.com>
2015-09-05 11:08:30 +0900

    no message

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var http=require('http');
+var servicekey = 'YWj8qq5V0WpoccMC8GOXs%2BCxVePxpCqq%2FftHFru%2Fum0NN6I7oHUbxIIPHW%2FSRPm1%2FUEJ6JM7xTeuPBU0zEldYQ%3D%3D';
+var goodid = 11;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET */
+router.get('/getdata', function (req, res){
+	var url = 'http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do?goodInspectDay=20150904&entpId=27&goodId='+goodid+'&ServiceKey='+servicekey;
+	if (url){
+		http.get(url, function (web){
+			web.on('data', function (buffer){
+				res.write(buffer);
+			});
+			web.on('end', function (){
+				res.end();
+			});
+		});
+	}else{
+		res.send('url error');
+	}
+});
+
+/* GET */
+router.get('/getdata', function (req, res){
+	var url = 'http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do?goodInspectDay=20150904&entpId=27&goodId='+goodid+'&ServiceKey='+servicekey;
+	if (url){
+		http.get(url, function (web){
+			web.on('data', function (buffer){
+				res.write(buffer);
+			});
+			web.on('end', function (){
+				res.end();
+			});
+		});
+	}else{
+		res.send('url error');
+	}
+});
+/* GET home page. */
+router.get('/goods', function(req, res, next) {
+  res.render('goods');
 });
 
 module.exports = router;
