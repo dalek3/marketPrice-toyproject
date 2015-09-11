96214c994bacf5e7c8f97f8de65c4fa9f5399b3a
dalek3 <lina1318@gmail.com>
2015-09-11 14:46:22 +0900

    오픈소스데이터가 너무 파편화 되있어서 DB생성 (데이터는 오픈소스데이터 일부사용)

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var http=require('http');
 var mysql = require('mysql');
+
+
 //데이터베이스와 연결합니다.
 var connection = mysql.createConnection({
 	host:'us-cdbr-iron-east-02.cleardb.net',
@@ -10,58 +12,7 @@ var connection = mysql.createConnection({
 	database: 'heroku_acd28e380746cfd'
 });
 
-
-var servicekey = 'YWj8qq5V0WpoccMC8GOXs%2BCxVePxpCqq%2FftHFru%2Fum0NN6I7oHUbxIIPHW%2FSRPm1%2FUEJ6JM7xTeuPBU0zEldYQ%3D%3D';
-/* GET */
-router.get('/getdata', function (req, res){
-	var url = 'http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do?goodInspectDay=20150904&entpId=2&ServiceKey='+servicekey;
-	if (url){
-		http.get(url, function (web){
-			web.on('data', function (buffer){
-				res.write(buffer);
-			});
-			web.on('end', function (){
-				res.end();
-			});
-		});
-	}else{
-		res.send('url error');
-	}
-});
-
-/* GET */
-router.get('/getdata1', function (req, res){
-	var url = 'http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductInfoSvc.do?ServiceKey='+servicekey;
-	if (url){
-		http.get(url, function (web){
-			web.on('data', function (buffer){
-				res.write(buffer);
-			});
-			web.on('end', function (){
-				res.end();
-			});
-		});
-	}else{
-		res.send('url error');
-	}
-});
-
-/* GET */
-router.get('/getdata2', function (req, res){
-	var url = 'http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getStoreInfoSvc.do?ServiceKey='+servicekey;
-	if (url){
-		http.get(url, function (web){
-			web.on('data', function (buffer){
-				res.write(buffer);
-			});
-			web.on('end', function (){
-				res.end();
-			});
-		});
-	}else{
-		res.send('url error');
-	}
-});
+connection.connect();
 
 /* GET goods 전제 */
 router.get('/goods', function(req, res, next) {
@@ -69,7 +20,6 @@ router.get('/goods', function(req, res, next) {
 });
 
 /* GET detail */
-
 
 /* GET food */
 router.get('/goods/food', function(req, res, next) {
@@ -86,9 +36,15 @@ router.get('/goods/etc', function(req, res, next) {
   res.render('goods');
 });
 
-/* GET map */
+/* GET market */
 router.get('/market', function(req, res, next) {
-  res.render('market');
+	connection.query('SELECT * from store', function(err, rows, fields) {
+		if (err) {
+			console.log('error: ', err);
+			throw err;
+		}
+		  res.render('market', {row: rows});
+	})
 });
 
 
