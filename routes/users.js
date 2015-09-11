3d2542d3a8e4db59639d8551b1c4e50299beec13
dalek3 <lina1318@gmail.com>
2015-09-11 19:19:57 +0900

    사이트 미사용시 mysql 자체에서 연결끊어버리는 문제 해결

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,43 @@ var mysql = require('mysql');
 
 
 //데이터베이스와 연결합니다.
-var connection = mysql.createConnection({
+var db_config = {
 	host:'us-cdbr-iron-east-02.cleardb.net',
 	user: 'b2f741162c7959',
 	password: '14e1591c',
 	database: 'heroku_acd28e380746cfd'
-});
+};
 
-connection.connect();
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 /* GET goods 전제 */
 router.get('/goods', function(req, res, next) {
-  	connection.query('SELECT goodName,goodPrice,entpName,detailMean from goods a,price b,store c WHERE a.goodId = b.goodId AND b.entpId=c.entpId', function(err, data, fields) {  		if (err) {
+  	connection.query('SELECT goodName,goodPrice,entpName,detailMean from goods a,price b,store c WHERE a.goodId = b.goodId AND b.entpId=c.entpId'
+  		, function(err, data, fields) { 
+  			if (err) {
 				console.log('error: ', err);
 				throw err;
 			}
