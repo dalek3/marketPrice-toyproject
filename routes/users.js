6a4b3c36f718776fc5cb578ddbd05f8e3e86339c
dalek3 <lina1318@gmail.com>
2015-09-12 09:16:26 +0900

    카테고리 링크

--- routes/users.js
+++ routes/users.js
@@ -15,36 +15,38 @@ var db_config = {
 var connection;
 
 function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
+	connection = mysql.createConnection(db_config); // Recreate the connection, since
+																									// the old one cannot be reused.
 
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
+	connection.connect(function(err) {              // The server is either down
+		if(err) {                                     // or restarting (takes a while sometimes).
+			console.log('error when connecting to db:', err);
+			setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+		}                                     // to avoid a hot loop, and to allow our node script to
+	});                                     // process asynchronous requests in the meantime.
+																					// If you're also serving http, display a 503 error.
+	connection.on('error', function(err) {
+		console.log('db error', err);
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+			handleDisconnect();                         // lost due to either server restart, or a
+		} else {                                      // connnection idle timeout (the wait_timeout
+			throw err;                                  // server variable configures this)
+		}
+	});
 }
 
 handleDisconnect();
 
+
 /* GET goods 전제 */
 router.get('/goods', function(req, res, next) {
-  	connection.query('SELECT goodName,goodPrice,entpName,detailMean from goods a,price b,store c WHERE a.goodId = b.goodId AND b.entpId=c.entpId'
-  		, function(err, data, fields) { 
-  			if (err) {
+	connection.query('SELECT goodName,goodPrice,entpName,detailMean from goods a,price b,store c WHERE a.goodId = b.goodId AND b.entpId=c.entpId'
+			, function(err, data, fields) { 
+				if (err) {
 				console.log('error: ', err);
 				throw err;
 			}
+		//res.send(data);
 		res.render('goods', {row: data});
 	});
 });
@@ -52,20 +54,40 @@ router.get('/goods', function(req, res, next) {
 /* GET detail */
 
 // GET food
-/*
-router.get('/goods/food', function(req, res, next) {
-  res.render('goods');
+router.get('/food', function(req, res, next) {
+	connection.query('SELECT goodName,goodPrice,entpName,detailMean,goodSmlclsCode FROM goods a,price b,store c WHERE a.goodId = b.goodId AND b.entpId=c.entpId AND a.goodSmlclsCode >=030100000 AND a.goodSmlclsCode < 030200000'
+			, function(err, data, fields) { 
+				if (err) {
+				console.log('error: ', err);
+				throw err;
+			}
+	res.render('goods', {row: data});
+	});
 });
 
 //GET mfood
-router.get('/goods/mfood', function(req, res, next) {
-  res.render('goods');
+router.get('/mfood', function(req, res, next) {
+	connection.query('SELECT goodName,goodPrice,entpName,detailMean,goodSmlclsCode FROM goods a,price b,store c WHERE a.goodId = b.goodId AND b.entpId=c.entpId AND a.goodSmlclsCode >=030200000 AND a.goodSmlclsCode < 030300000'
+			, function(err, data, fields) { 
+				if (err) {
+				console.log('error: ', err);
+				throw err;
+			}
+	res.render('goods', {row: data});
+	});
 });
 
-///GET etc 
-router.get('/goods/etc', function(req, res, next) {
-  res.render('goods');
-});*/
+//GET etc 
+router.get('/etc', function(req, res, next) {
+	connection.query('SELECT goodName,goodPrice,entpName,detailMean,goodSmlclsCode FROM goods a,price b,store c WHERE a.goodId = b.goodId AND b.entpId=c.entpId AND a.goodSmlclsCode >=030300000'
+			, function(err, data, fields) { 
+				if (err) {
+				console.log('error: ', err);
+				throw err;
+			}
+	res.render('goods', {row: data});
+	});
+});
 
 /* GET market */
 router.get('/market', function(req, res, next) {
@@ -74,9 +96,8 @@ router.get('/market', function(req, res, next) {
 			console.log('error: ', err);
 			throw err;
 		}
-		  res.render('market', {row: rows});
+			res.render('market', {row: rows});
 	})
 });
-
 
 module.exports = router;
