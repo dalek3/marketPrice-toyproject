cd72476b9c1f5cd8d629013b6b942ce011ac18b2
dalek3 <lina1318@gmail.com>
2015-09-21 21:14:35 +0900

    무한스크롤 추가

--- routes/users.js
+++ routes/users.js
@@ -35,6 +35,10 @@ function handleDisconnect() {
 
 handleDisconnect();
 
+var start = 16;
+var offset = 28;//item per page
+var refreshoffset = 16;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', {title: '뭐살까'});
@@ -43,27 +47,47 @@ router.get('/', function(req, res, next) {
 /*검색기능*/
 router.get('/search', function(req, res, next) {
 	var q= req.query.search;
-	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodName LIKE "%'+req.query.q+'%"'
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodName LIKE "%'+req.query.q+'%" LIMIT 0, ?'
+		, [refreshoffset]
 		,function(err, data, fields) { 
 			if (err) {
 				console.log('error: ', err);
 				throw err;
 			} 
-			console.log(q); 
+			start = 0;
 			res.render('search', {title: '뭐살까',row: data});
 		});
 });
 
+router.get('/api/search', function(req, res, next) {
+	start += refreshoffset + 1;
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodName LIKE "%'+req.query.q+'%" LIMIT ?, ?'
+		, [start, refreshoffset], function (err, data1) {
+		console.log(start + '::' + refreshoffset);
+		res.send(data1);
+	});
+});
+
 /* GET goods 전제 */
 router.get('/goods', function(req, res, next) {
-	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code'
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code LIMIT 0, ?'
+		, [refreshoffset]
 		, function(err, data, fields) { 
 			if (err) {
 				console.log('error: ', err);
 				throw err;
 			}
+			start = 0;
 			res.render('goods', {title: '뭐살까',row: data});
 		});
+});
+
+router.get('/api/goods', function(req, res, next) {
+	start += refreshoffset + 1;
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code LIMIT ?, ?', [start, refreshoffset], function (err, data1) {
+		console.log(start + '::' + refreshoffset);
+		res.send(data1);
+	});
 });
 
 /* GET goods detail */
@@ -85,38 +109,68 @@ router.get('/goodsView', function(req, res, next) {
 
 // GET food
 router.get('/food', function(req, res, next) {
-	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030100000 AND goodSmlclsCode < 030200000'
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030100000 AND goodSmlclsCode < 030200000 LIMIT 0, ?'
+		, [refreshoffset]
 		, function(err, data, fields) { 
 			if (err) {
 				console.log('error: ', err);
 				throw err;
-			} 
-			res.render('goods', {title: '뭐살까',row: data});
+			}
+			start = 0;
+			res.render('food', {title: '뭐살까',row: data});
 		});
+});
+
+router.get('/api/food', function(req, res, next) {
+	start += refreshoffset + 1;
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030100000 AND goodSmlclsCode < 030200000 LIMIT ?, ?', [start, refreshoffset], function (err, data1) {
+		console.log(start + '::' + refreshoffset);
+		res.send(data1);
+	});
 });
 
 //GET mfood
 router.get('/mfood', function(req, res, next) {
-	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030200000 AND goodSmlclsCode < 030300000'
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030200000 AND goodSmlclsCode < 030300000 LIMIT 0, ?'
+		, [refreshoffset]
 		, function(err, data, fields) { 
 			if (err) {
 				console.log('error: ', err);
 				throw err;
 			}
-			res.render('goods', {title: '뭐살까',row: data});
+			start = 0;
+			res.render('mfood', {title: '뭐살까',row: data});
 		});
+});
+
+router.get('/api/mfood', function(req, res, next) {
+	start += refreshoffset + 1;
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030200000 AND goodSmlclsCode < 030300000 LIMIT ?, ?', [start, refreshoffset], function (err, data1) {
+		console.log(start + '::' + refreshoffset);
+		res.send(data1);
+	});
 });
 
 //GET etc 
 router.get('/etc', function(req, res, next) {
-	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030300000'
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030300000 LIMIT 0, ?'
+		, [refreshoffset]
 		, function(err, data, fields) { 
 			if (err) {
 				console.log('error: ', err);
 				throw err;
 			}
-			res.render('goods', {title: '뭐살까',row: data});
+			start = 0;
+			res.render('etc', {title: '뭐살까',row: data});
 		});
+});
+
+router.get('/api/etc', function(req, res, next) {
+	start += refreshoffset + 1;
+	connection.query('SELECT * FROM goods a, store b,newprice c, standard d WHERE a.goodId=c.goodId AND b.entpId=c.entpId AND a.goodTotalDivCode=d.code AND goodSmlclsCode >=030300000 LIMIT ?, ?', [start, refreshoffset], function (err, data1) {
+		console.log(start + '::' + refreshoffset);
+		res.send(data1);
+	});
 });
 
 /* GET market */
@@ -142,6 +196,8 @@ router.get('/marketView', function(req, res, next) {
 			res.render('marketDetail', {title: '뭐살까',row: data});
 		})
 });
+
+
 
 
 module.exports = router;
